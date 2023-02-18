6b94c11acc59f44c5ec96067b731d4e94b24078e
Paula Welz <paula.welz@student.kit.edu>
2023-02-18 17:21:47 +0100

    Add testset for free rewrite

--- WhiteheadAlgorithm/test/runtests.jl
+++ WhiteheadAlgorithm/test/runtests.jl
@@ -15,3 +15,14 @@ WHA = WhiteheadAlgorithm
     @test_throws ArgumentError WHA.inv(A,2)
     @test_throws AssertionError WHA.setinverse!(A, 2, 3)
 end
+
+@testset "free rewrite" begin
+    A = WHA.Alphabet{Char}(['x','X','y','Y'])
+    WHA.setinverse!(A,1,2)
+    WHA.setinverse!(A,3,4)
+    @test WHA.freerewrite(A, []) == []
+    @test WHA.freerewrite(A, [1,2]) == []
+    @test WHA.freerewrite(A, [1,4,3,2,2]) == [2]
+    @test WHA.freerewrite(A, [1,3,2,4]) == [1,3,2,4]
+    @test_throws AssertionError WHA.freerewrite(A, [5])
+end
